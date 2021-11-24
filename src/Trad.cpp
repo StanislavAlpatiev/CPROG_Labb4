3016ca639748883dee3a44b9d6c7ba4dd005d0a9
StanislavAlpatiev <alpatiev.stanislav@gmail.com>
2021-11-24 13:21:16 +0100

    moved implementation of methods into header file

--- src/Trad.cpp
+++ src/Trad.cpp
@@ -2,109 +2,102 @@
 //  Trad.cpp
 //  labb4_tree
 
-#include <iostream>
-#include "Trad.h"
-template <typename T>
-void Trad<T>::kopiera(const Trad<T> &t)
-{
-    //std::cout << "*** Trad::kopiera\n";
-    if (t.tomt())
-        rot = 0;
-    else
-    {
-        rot = new Nod(t.varde());
-        v_barn().kopiera(t.v_barn());
-        h_barn().kopiera(t.h_barn());
-    }
-}
+// #include <iostream>
+// #include <cassert>
+// #include "Trad.h"
 
-template <typename T>
-Trad<T> &Trad<T>::operator=(const Trad<T> &t)
-{
-    //std::cout << "*** Trad::operator=\n";
-    if (rot != t.rot)
-    {
-        delete rot;
-        kopiera(t);
-    }
-    return *this;
-}
+// template <class T>
+// void Trad<T>::kopiera(const Trad<T> &t)
+// {
+//     //std::cout << "*** Trad::kopiera\n";
+//     if (t.tomt())
+//         rot = 0;
+//     else
+//     {
+//         rot = new Nod(t.varde());
+//         v_barn().kopiera(t.v_barn());
+//         h_barn().kopiera(t.h_barn());
+//     }
+// }
 
-template <typename T>
-bool Trad<T>::operator==(const Trad<T> &t) const
-{
-    //std::cout << "*** Trad::operator==\n";
-    return (tomt() && t.tomt()) ||
-           (!tomt() && !t.tomt() && varde() == t.varde() &&
-            v_barn() == t.v_barn() && h_barn() == t.h_barn());
-}
+// template <class T>
+// Trad<T> &Trad<T>::operator=(const Trad<T> &t)
+// {
+//     //std::cout << "*** Trad::operator=\n";
+//     if (rot != t.rot)
+//     {
+//         delete rot;
+//         kopiera(t);
+//     }
+//     return *this;
+// }
 
-template <typename T>
-void Trad<T>::skriv_ut() const
-{
-    // traversera igenom trädet rekursivt enligt principen "in-order"
-    if (!tomt())
-    {
-        v_barn().skriv_ut();
-        std::cout << varde() << ' ';
-        h_barn().skriv_ut();
-    }
-}
+// template <class T>
+// bool Trad<T>::operator==(const Trad<T> &t) const
+// {
+//     //std::cout << "*** Trad::operator==\n";
+//     return (tomt() && t.tomt()) ||
+//            (!tomt() && !t.tomt() && varde() == t.varde() &&
+//             v_barn() == t.v_barn() && h_barn() == t.h_barn());
+// }
 
-template <typename T>
-void Trad<T>::satt_in(const T &value)
-{
-    if (tomt)
-    {
-        Trad(value);
-    }
-    else
-    {
-        if (sok(value))
-        {
-            navigateDownTree(value);
-        }
-    }
-}
+// template <class T>
+// void Trad<T>::skriv_ut() const
+// {
+//     // traversera igenom trädet rekursivt enligt principen "in-order"
+//     if (!tomt())
+//     {
+//         v_barn().skriv_ut();
+//         std::cout << varde() << ' ';
+//         h_barn().skriv_ut();
+//     }
+// }
 
-template <typename T>
-void Trad<T>::navigateDownTree(const T &value)
-{
-    if (value < rot->data && rot->vanster->rot->data == 0)
-    {
-        rot->vanster = new Trad(value);
-    }
-    else if (value > rot->data && rot->hoger->rot->data == 0)
-    {
-        rot->hoger = new Trad(value);
-    }
-    else if (value != rot->data && value < rot->data)
-    {
-        rot->vanster->navigateDownTree(value);
-    }
-    else if (value != rot->data && value > rot->data)
-    {
-        rot->hoger->navigateDownTree(value);
-    }
-}
+// template <class T>
+// void Trad<T>::satt_in(const T &value)
+// {
+//     if (tomt())
+//     {
+//         rot = new Nod(value);
+//     }
+//     else
+//     {
+//         if (value < rot->data)
+//         {
+//             v_barn().satt_in(value);
+//         }
+//         else if (value == rot->data)
+//         {
+//             return;
+//         }
+//         else
+//         {
+//             h_barn().satt_in(value);
+//         }
+//     }
+// }
 
-template <typename T>
-bool Trad<T>::sok(const T &value)
-{
-    if (rot->vanster->rot->data == 0 && rot->vanster->rot->data == 0 && value != rot->data)
-    {
-        return 0;
-    }
-    else if (value == rot->data)
-    {
-        return 1;
-    }
-    else if (value != rot->data && rot->vanster && value < rot->data)
-    {
-        rot->vanster->sok(value);
-    }
-    else if (value != rot->data && rot->hoger != 0 && value > rot->data)
-    {
-        rot->hoger->sok(value);
-    }
-}
+// template <class T>
+// T Trad<T>::sok(const T &value) const
+// {
+//     if (tomt())
+//     {
+//         return 0;
+//     }
+//     else if (value == rot->data)
+//     {
+//         return value;
+//     }
+//     else if (value < rot->data)
+//     {
+//         return v_barn().sok(value);
+//     }
+//     else
+//     {
+//         return h_barn().sok(value);
+//     }
+// }
+
+// template class Trad<double>;
+// template class Trad<int>;
+// template class Trad<short>;
--- src/Trad.h
+++ src/Trad.h
@@ -6,11 +6,11 @@
 #define TRAD_H
 
 #include <iostream>
+#include <cassert>
 
 template <typename T>
 class Trad
 {
-
 public:
     Trad() : rot(0){}; // default konstruktor
     Trad(T d) { rot = new Nod(d); };
@@ -18,7 +18,7 @@ public:
     ~Trad() { delete rot; };
 
     bool tomt() const { return !rot; };
-    int &varde() const
+    T &varde() const
     {
         koll();
         return rot->data;
@@ -38,13 +38,12 @@ public:
     bool operator==(const Trad &) const;
     void skriv_ut() const;
     void satt_in(const T &value);
-    bool sok(const T &value);
+    T sok(const T &value) const;
 
 private:
-    template <typename T>
     class Nod
     {
-        friend class Trad;
+        friend class Trad<T>;
         T data;
         Trad *vanster, *hoger;
         Nod(T d) : data(d), vanster(new Trad), hoger(new Trad){};
@@ -62,8 +61,98 @@ private:
             throw std::range_error("Trad");
     }
     void kopiera(const Trad &t);
-    void navigateDownTree(const T &value);
-    void navigateTreeRoot();
 };
+
+template <typename T>
+void Trad<T>::kopiera(const Trad<T> &t)
+{
+    //std::cout << "*** Trad::kopiera\n";
+    if (t.tomt())
+        rot = 0;
+    else
+    {
+        rot = new Nod(t.varde());
+        v_barn().kopiera(t.v_barn());
+        h_barn().kopiera(t.h_barn());
+    }
+}
+
+template <typename T>
+Trad<T> &Trad<T>::operator=(const Trad<T> &t)
+{
+    //std::cout << "*** Trad::operator=\n";
+    if (rot != t.rot)
+    {
+        delete rot;
+        kopiera(t);
+    }
+    return *this;
+}
+
+template <typename T>
+bool Trad<T>::operator==(const Trad<T> &t) const
+{
+    //std::cout << "*** Trad::operator==\n";
+    return (tomt() && t.tomt()) ||
+           (!tomt() && !t.tomt() && varde() == t.varde() &&
+            v_barn() == t.v_barn() && h_barn() == t.h_barn());
+}
+
+template <typename T>
+void Trad<T>::skriv_ut() const
+{
+    // traversera igenom trädet rekursivt enligt principen "in-order"
+    if (!tomt())
+    {
+        v_barn().skriv_ut();
+        std::cout << varde() << ' ';
+        h_barn().skriv_ut();
+    }
+}
+
+template <typename T>
+void Trad<T>::satt_in(const T &value)
+{
+    if (tomt())
+    {
+        rot = new Nod(value);
+    }
+    else
+    {
+        if (value < rot->data)
+        {
+            v_barn().satt_in(value);
+        }
+        else if (value == rot->data)
+        {
+            return;
+        }
+        else
+        {
+            h_barn().satt_in(value);
+        }
+    }
+}
+
+template <typename T>
+T Trad<T>::sok(const T &value) const
+{
+    if (tomt())
+    {
+        return 0;
+    }
+    else if (value == rot->data)
+    {
+        return value;
+    }
+    else if (value < rot->data)
+    {
+        return v_barn().sok(value);
+    }
+    else
+    {
+        return h_barn().sok(value);
+    }
+}
 
 #endif /* Trad_h */
